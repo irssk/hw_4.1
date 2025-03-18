98eb2ba10447828f7a2d0239276dc18765c96279
Ira <ira350520@gmail.com>
2025-03-18 13:36:09 +0200

    completed homework

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the day of the week number (1-7): ");
+
+        if (int.TryParse(Console.ReadLine(), out int dayNumber))
+        {
+            string dayName = dayNumber switch
+            {
+                1 => "Monday",
+                2 => "Tuesday",
+                3 => "Wednesday",
+                4 => "Thursday",
+                5 => "Friday",
+                6 => "Saturday",
+                7 => "Sunday",
+                _ => "Error: Please enter a number between 1 and 7!"
+            };
+
+            Console.WriteLine(dayName);
+        }
+        else
+        {
+            Console.WriteLine("Error: Please enter a valid number!");
+        }
+    }
+}
